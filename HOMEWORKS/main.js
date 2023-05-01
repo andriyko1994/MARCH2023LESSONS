bc012c1aac21e9cd23e1e17bbdefd4dfbb3ae90b
Andriy Kohut <iandrosuk@icloud.com>
2023-05-02 00:23:22 +0300

    hw1.js

---
+++ HOMEWORKS/main.js
@@ -0,0 +1,52 @@
+let hello = `hello`
+console.log(hello)
+let owu = `owu`
+console.log(owu)
+let com = `com`
+console.log(com)
+let ua = `ua`
+console.log(ua)
+let a = 1
+console.log(1)
+let b = 10
+console.log(10)
+let c = -999
+console.log(-999)
+let d = 123
+console.log(123)
+let e = 3.14
+console.log(3.14)
+let f = 2.7
+console.log(2.7)
+let g = 16
+console.log(16)
+let check = `true`
+console.log(true)
+let check = `false`
+console.log(false)
+
+
+
+let firstName = `Kohut`
+let middleName = `Andriy`
+let lastName = `Bohdanowich`
+let person = firstName + `Kohut` + middleName + `Andriy` + lastName + `Bohdanowich`
+
+
+let a = 100;
+console.log(100);
+let b = '100';
+console.log(100);
+let c = true;
+console.log(true);
+typeof a b c = 100 + `100` + true;
+
+
+let Name = prompt(`Andriy`);
+let lastName = prompt(`Bohdanowich`);
+let myAge = prompt(28);
+
+console.log(`Andriy`);
+console.log(`Bohdanowich`);
+console.log(28)
+
